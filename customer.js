bc7dfcdd17a67d9bf1223f03f0deb11ab125f1df
dcsab <dcsab@nus.edu.sg>
2022-08-18 21:44:44 +0800

    Dockerfile

--- customer.js
+++ customer.js
@@ -32,6 +32,21 @@ router.get("/customer/id", (request, response) => {
   );
 });
 
+// defines an API which takes id & password in the request and return success of fail
+/* router.get("/customer/login", (request, response) => {
+  sqlst = `select * from customer where customer_id = ${request.query.cid} & password = ${request.query.pwd}` ; 
+  database.connection.all(
+    sqlst,
+    (errors, results) => {
+      if (errors) {
+        response.status(500).send("Some error occurred or invalid user id or password ");
+      } else {
+        response.status(200).send("success");
+      }
+    }
+  );
+}); */
+
 // a POST API to store the record received in the request
 router.post("/customer/add", (request, response) => {
   database.connection.all(
